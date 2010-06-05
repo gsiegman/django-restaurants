136a812a27475e1b7d297cf1f97111986be12119
Glenn Siegman <gsiegman@gsiegman.com>
2010-06-05 17:19:13 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='django-restaurants',
+        version='0.1',
+        description='A django app to manage restaurant information and menus.',
+        author='Glenn Siegman',
+        author_email='gsiegman@gsiegman.com',
+        url='http://github.com/gsiegman/django-restaurants',
+        packages=['restaurants']
+)
